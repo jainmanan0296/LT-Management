336576625dae862cdc014166d0e5062c94408797
Manan Jain <jainmanan806@gmail.com>
2024-09-14 16:56:51 +0530

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,13 @@ import App from './App.jsx';
 import store from './store'; // Import the store configured with Redux Toolkit
 import './index.css';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+//import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </StrictMode>
 );
 
